adbf098e5f3f350d3aaaa57f805d81310be88ba1
Iryney <iryney.b@gmail.com>
2015-11-07 19:26:50 +0200

    init

--- public/core.js
+++ public/core.js
@@ -3,7 +3,7 @@ var irkoTodo = angular.module('irkoTodo', []);
 function mainController($scope, $http) {
     $scope.formData = {};
 
-    // when landing on the page, get all todos and show them
+
     $http.get('/api/todos')
         .success(function(data) {
             console.log(data);
@@ -13,11 +13,11 @@ function mainController($scope, $http) {
             console.log('Error: ' + data);
         });
 
-    // when submitting the add form, send the text to the node API
+
     $scope.createTodo = function() {
         $http.post('/api/todos', $scope.formData)
             .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.formData = {}; 
                 $scope.todos = data;
                 console.log(data);
             })
@@ -26,7 +26,7 @@ function mainController($scope, $http) {
             });
     };
 
-    // delete a todo after checking it
+
     $scope.deleteTodo = function(id) {
         $http.delete('/api/todos/' + id)
             .success(function(data) {
