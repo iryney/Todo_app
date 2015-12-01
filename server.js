81b180484180c164816ca3e891ea2e2370067285
Iryney <iryney.b@gmail.com>
2015-12-01 21:14:52 +0200

    Add comments.

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 
 mongoose.connect('mongodb://localhost/todo');
 app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-app.get('#', function(req,res){
-	res.sendfile('./public/index.html');
-});
-
 app.listen(8080);
 console.log("App listening on port 8080");
 
-
+//schema for database
 var Todo = mongoose.model('Todo', {
 	text: String,
 	date: Date,
 	done: Boolean
 });
 
+
+//routing
+app.get('#', function(req,res){
+	res.sendfile('./public/index.html');
+});
+
+//get todos
 app.get('/api/todos', function(req, res){
 	Todo.find(function(err, todos){
 		if(err)
@@ -36,6 +37,7 @@ app.get('/api/todos', function(req, res){
 	});
 });
 
+//post new todo
 app.post('/api/todos', function(req, res){
 	Todo.create({
 		text: req.body.text,
@@ -52,8 +54,7 @@ app.post('/api/todos', function(req, res){
 	});
 });
 
-
-
+//delete todo from database
 app.delete('/api/todos/:todo_id', function(req, res) {
         Todo.remove({
             _id : req.params.todo_id
@@ -61,11 +62,10 @@ app.delete('/api/todos/:todo_id', function(req, res) {
             if (err)
                 res.send(err);
 
-            // get and return all the todos after you create another
             Todo.find(function(err, todos) {
                 if (err)
                     res.send(err)
                 res.json(todos);
             });
         });
-    });
+});
